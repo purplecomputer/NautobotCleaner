a1eaf13160b29132586f905ac013c0299957873f
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-04-15 14:57:17 -0400

    re-worked vlan dict mapping and some other crap

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from config import nautobot_url, nautobot_token, device_platform_connection,device_username,device_password
 import pynautobot
 import napalm
+import pprint
 import gevent
 from gevent import monkey
 
@@ -32,78 +33,93 @@ class NautobotCleaner:
         self.device_init.open()
         return self.device_init.get_vlans()
 
-
-    def nautobotvlanimport(self,group,vlans):
-        '''dumps them vlans into them groups and links it to the SVI created'''
-        #did you give me good data?
-        if not isinstance(vlans, dict):
-            raise("Vlans is not a dict")
-
-        #Check that group exsists & create it if it dont
+    def _formatnapalmvlandict(self, group,vlans):
+        newdict = {}
+        #find the group cause you'll need it later
+        # Check that group exsists & create it if it dont
         vlangroup = self.pynb.ipam.vlan_groups.get(name=str(group))
         if vlangroup is None:
             vlangroup = self.pynb.ipam.vlan_groups.create(
                 name=str(group)
             )
-
-        for vid,vidinfo in vlans.items():
-            vlan = self.pynb.ipam.vlans.get(
-                vid=str(vid),
-                group_id=vlangroup.id
+        if not isinstance(vlans, dict):
+            raise Exception("vlan arg must be a dictionary")
+        for k, v in vlans.items():
+            vlanid = self.pynb.ipam.vlans.get(
+                vid=str(k),
+                vlan_group=vlangroup.id
             )
-            if vlan is None:
-                vlan = self.pynb.ipam.vlans.create(
-                    name=str(vid),
-                    vid=vid,
+            if vlanid is None:
+                vlanid = self.pynb.ipam.vlans.create(
+                    name=str(k),
+                    vid=k,
                     group=str(vlangroup.id),
                     site=self.device.site.id,
-                    status='active'
+                    status='active',
+                    description=j['name']
                 )
-            #Tie vlan to the interface
-            for interfaces in vidinfo['interfaces']:
-                interface = self.pynb.dcim.interfaces.get(
-                    name=str(interfaces),
-                    device_id = self.device.id
-                )
-                print(interface.mode)
-                if interface is None:
-                    print("interface not on device - skipping")
-                    continue
-                elif interface.mode == None:
+            for j in v['interfaces']:
+                if j in newdict:
+                    #if the key is already there add the vlan
+                    newdict[j].append(vlanid.id)
+                else:
+                    newdict[j] = [vlanid.id]
+        return(newdict)
+
+    def nautobotvlanimport(self,group,vlans):
+        '''dumps them vlans into them groups and links it to the SVI created'''
+
+        #did you give me good data?
+        if not isinstance(vlans, dict):
+            raise("Vlans is not a dict")
+
+        #convert the Dict to something thats easier to use here
+        vlans = self._formatnapalmvlandict(group,vlans)
+        #vlangroup = self.pynb.ipam.vlan_groups.get(name=str(group))
+
+        for interface,vlan in vlans.items():
+            '''query interface object'''
+            interfaceQuery = self.pynb.dcim.interfaces.get(
+                name=str(interface),
+                device_id=self.device.id
+            )
+            if interfaceQuery is None:
+                print(f'Interface: {interface} does not match SOT list - Skipping!')
+                continue
+            if len(vlan) == 1:
+                if interfaceQuery.mode.value == None:
                     '''if the interface exsist but has no vlans or mode set
                     update and link curent vlan to vlan we are
                     set the interface as access'''
                     print('setting int as untagged')
-                    interface.update(
-                        mode = 'Access',
-                        untagged_vlan= vlan.id
-                    )
-                elif interface.mode == 'Access' and interface.untagged_vlan is None:
-                    '''if the interface exsists and was set untagged by network importer w/ no vlans'''
+                    interface.update({
+                        'mode' : 'access',
+                        'untagged_vlan' : vlan[0]
+                    })
+                elif interfaceQuery.untagged_vlan is None:
+                    '''if the interfaceQuery exsists and was set untagged by network importer w/ no vlans'''
                     print('linking vlan to untagged int')
-                    interface.update(
-                        mode = 'Access',
-                        untagged_vlans = vlan.id
-                    )
-                elif interface.mode == 'Access' and interface.untagged_vlan is not None:
-                    '''if the interface exsists and it currently has an untagged vlan'''
-                    print('int has more than one vlan configured - setting to tagged')
-                    interface.update(
-                        mode = 'Tagged',
-                        tagged_vlans = vlan.id
-                    )
-                elif interface.mode == 'Tagged':
-                    '''if interface exsist and already has more than two vlans
-                    update with additional vlan tag'''
-                    print('adding additional vlan to tagged int')
-                    interface.update(
-                        tagged_vlans = vlan.id
-                    )
-
-
+                    interfaceQuery.update({
+                        'untagged_vlan' :  vlan[0]
+                    })
+                else:
+                    '''if logic doesnt match just set the mode to access and link VLAN'''
+                    print('nothing matches, setting as access w/ ID')
+                    interfaceQuery.update({
+                        'mode':          'access',
+                        'untagged_vlan': vlan[0]
+                    })
+            else:
+                '''If vlan dict value list is longer than 1'''
+                interfaceQuery.update({
+                    'mode' : 'tagged',
+                    'tagged_vlans' : vlan
+                })
 
 if __name__ == "__main__":
     nbc = NautobotCleaner('dsc121.gsc.webair.net')
     vlan_data = nbc.getvlans()
+    #test = nbc._formatnapalmvlandict('ds121', vlan_data)
+    #pprint.pprint(test)
     nbc.nautobotvlanimport('ds121', vlan_data)
 
